5558b97a30a83f9a2422d28705222f954b0155c5
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-11-04 18:40:06 +0530

    Clean up the default project.

--- app/src/main/java/com/getswipe/textmatic/SecondFragment.kt
+++ app/src/main/java/com/getswipe/textmatic/SecondFragment.kt
@@ -8,30 +8,22 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.getswipe.textmatic.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
